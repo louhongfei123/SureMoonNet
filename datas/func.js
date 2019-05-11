9406724b70f07618f8d3db818b808fc0502cb7cc
suirenjie <guyvejianglou@outlook.com>
2019-05-11 17:16:19 +0800

    PGLang architecture related

---
+++ datas/func.js
@@ -0,0 +1,19 @@
+function _() {
+    return "{{"
+}
+
+function _n(n) {
+    var res = ""
+    for(var i = 0; i < n; i++){
+        res += "{"
+    }
+    return res
+}
+
+function test(arg) {
+    return "js function return :" + arg
+}
+
+function test2(arg) {
+    return "js function test2"
+}
